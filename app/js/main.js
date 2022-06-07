1a841b13dc7e364f28a73b02411ea95f5010ab37
k2k-com <k2k_com@mail.ru>
2022-06-07 19:46:42 +0300

    added surf

--- app/js/main.js
+++ app/js/main.js
@@ -17,9 +17,20 @@ $(function () {
     });
 
     $('.surf-slider').slick({
-        slidesToShow: 1,
+        centerMode: true,
+        slidesToScroll: 1,
         slidesToShow: 4,
         prevArrow: '<button class="slick-prev slick-arrow"><img src="../images/icons/slick-prev.svg" alt=""></button>',
         nextArrow: '<button class="slick-next slick-arrow"><img class="left" src="../images/icons/slick-next.svg" alt=""></button>',
+        asNavFor: '.slider__map'
+    });
+
+    $('.slider__map').slick({
+        arrows: false,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
+
     });
 });
