556fa8cb8eb11f8798fd87e3e044092f72ad9131
k2k-com <k2k_com@mail.ru>
2022-06-10 14:03:51 +0300

    travei added

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,6 @@ $(function () {
 
     $('.slider-dots').slick({
         slidesToShow: 4,
-        slidesToShow: 4,
         asNavFor: '.header__slider',
     });
 
@@ -22,7 +21,8 @@ $(function () {
         slidesToShow: 4,
         prevArrow: '<button class="slick-prev slick-arrow"><img src="../images/icons/slick-prev.svg" alt=""></button>',
         nextArrow: '<button class="slick-next slick-arrow"><img class="left" src="../images/icons/slick-next.svg" alt=""></button>',
-        asNavFor: '.slider__map'
+        asNavFor: '.surf__subtitle',
+        asNavFor: '.slider__map',
     });
 
     $('.slider__map').slick({
@@ -30,7 +30,27 @@ $(function () {
         slidesToShow: 8,
         slidesToScroll: 1,
         asNavFor: '.surf-slider',
+        asNavFor: '.surf__subtitle',
         focusOnSelect: true
 
+    });
+    $('.surf__subtitle').slick({
+        asNavFor: '.slider__map', 
+        asNavFor: '.surf-slider',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false
+    });
+
+    $('.travel__slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        fade: true,
+        asNavFor: '.slider-dots',
+        prevArrow: '<button class="slick-prev slick-arrow"><img src="../images/icons/slick-prev.svg" alt=""></button>',
+        nextArrow: '<button class="slick-next slick-arrow"><img class="left" src="../images/icons/slick-next.svg" alt=""></button>',
     });
 });
